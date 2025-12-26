b7e10ae7c537f27c16df5ae50c3b4e1be16d1205
Daniel <danielptm@me.com>
2025-12-26 15:47:47 -0800

    Added bringup package and more config/launch files

---
+++ bringup.sh
@@ -0,0 +1,2 @@
+source ~/.bashrc
+ros2 launch yardguard_bringup yardguard_gazebo.launch.yaml
--- build_xacro.sh
+++ build_xacro.sh
@@ -1,1 +1,1 @@
-ros2 run xacro xacro -o /home/daniel/projects/yard_guard/src/yardguard_description/urdf/yardguard.urdf /home/daniel/projects/yard_guard/src/yardguard_description/urdf/xacro/yardguard.xacro;
+ros2 run xacro xacro -o /home/daniel/projects/yard_guard/src/yardguard_description/urdf/xacro/yardguard.xacro /home/daniel/projects/yard_guard/src/yardguard_description/urdf/yardguard.urdf;
