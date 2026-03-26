644979ded7bb87a217f1da85364b2b86d7affba4
Daniel <foodlikeair@icloud.com>
2026-03-25 21:16:18 -0700

    added maps and better bash script for starting with map, still does not work

---
+++ start_map_usecase.sh
@@ -0,0 +1,23 @@
+gnome-terminal -- bash -c "bash build.sh; bash bringup.sh;"
+sleep 15;
+sleep 1;
+gnome-terminal -- bash -c "ros2 run teleop_twist_keyboard teleop_twist_keyboard"
+sleep 1;
+gnome-terminal -- bash -c "ros2 launch nav2_bringup navigation_launch.py use_sim_time:=True map:=/home/daniel/projects/foodlikeair/2wd_robot/maps/test_map/map.yaml"
+sleep 1;
+gnome-terminal -- bash -c "ros2 run nav2_util lifecycle_bringup map_server"
+
+
+
+#sleep 1000;
+# echo "hey!!!!"
+
+
+# gnome-terminal -- bash -c "ros2 launch slam_toolbox online_async_launch.py use_sim_time:=true"
+
+
+
+
+
+
+# ros2 launch nav2_bringup navigation2.launch.py use_sim_time:=True map:=/path/to/your/map.yaml
