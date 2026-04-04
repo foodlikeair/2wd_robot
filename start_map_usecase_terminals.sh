557ea9342507422fcf534134913ec92d315b117b
daniel <foodlikeair@icloud.com>
2026-04-04 14:35:10 -0700

    fixed the robot model not connected to tf map/odom

--- start_map_usecase_terminals.sh
+++ start_map_usecase_terminals.sh
@@ -13,9 +13,11 @@ sleep 2
 gnome-terminal -- bash -c "ros2 run rviz2 rviz2 -d /home/daniel/projects/foodlikeair/2wd_robot/src/yardguard_description/rviz/map_loaded.rviz"
 
 # sleep 2
-# # gnome-terminal -- bash -c "ros2 launch slam_toolbox online_async_launch.py use_sim_time:=true"
-# sleep 25
+# gnome-terminal -- bash -c "ros2 launch slam_toolbox online_async_launch.py use_sim_time:=True" map_file:=/home/daniel/projects/foodlikeair/2wd_robot/maps/test_map/map.yaml
+
+# sleep 2
 # gnome-terminal -- bash -c "ros2 run nav2_map_server map_server --ros-args --params-file /home/daniel/projects/foodlikeair/2wd_robot/maps/test_map/map_server_params.yaml"
+
 # sleep 3
 # gnome-terminal -- bash -c "ros2 run nav2_util lifecycle_bringup map_server use_sim_time:=True"
 
