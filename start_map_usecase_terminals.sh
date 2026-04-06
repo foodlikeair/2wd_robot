266d6d182d41276db93b5a212f83765e943bb640
Daniel <danielptm@me.com>
2026-04-05 19:59:21 -0700

    made progress

--- start_map_usecase_terminals.sh
+++ start_map_usecase_terminals.sh
@@ -9,8 +9,10 @@ gnome-terminal -- bash -c "ros2 run teleop_twist_keyboard teleop_twist_keyboard"
 sleep 2
 gnome-terminal -- bash -c "ros2 launch nav2_bringup bringup_launch.py use_sim_time:=True map:=/home/daniel/projects/foodlikeair/2wd_robot/maps/test_map/map.yaml"
 sleep 2
-# gnome-terminal -- bash -c "ros2 run rviz2 rviz2 -d /home/daniel/projects/foodlikeair/2wd_robot/src/yardguard_description/rviz/urdf_config.rviz"
-gnome-terminal -- bash -c "ros2 run rviz2 rviz2 -d /home/daniel/projects/foodlikeair/2wd_robot/src/yardguard_description/rviz/map_loaded.rviz"
+
+# gnome-terminal -- bash -c "ros2 run rviz2 rviz2 --ros-args -p use_sim_time:=true -d /home/daniel/projects/foodlikeair/2wd_robot/src/yardguard_description/rviz/map_loaded.rviz"
+
+gnome-terminal -- bash -c "ros2 run rviz2 rviz2 use_sim_time:=true -d /home/daniel/projects/foodlikeair/2wd_robot/src/yardguard_description/rviz/map_loaded.rviz"
 
 # sleep 2
 # gnome-terminal -- bash -c "ros2 launch slam_toolbox online_async_launch.py use_sim_time:=True" map_file:=/home/daniel/projects/foodlikeair/2wd_robot/maps/test_map/map.yaml
